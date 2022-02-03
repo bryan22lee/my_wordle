989b355a1829d708bdddb483c7c0086c852efc4d
bryan22lee <bryan22lee@gmail.com>
2022-02-03 00:21:26 -0600

    finished and tested

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-Command-line version of [Wordle](https://www.powerlanguage.co.uk/wordle/) (in progress)
+## Command-line version of [Wordle](https://www.powerlanguage.co.uk/wordle/)
 
-`python3 my_wordle.py`
+Run with `python3 my_wordle.py`
+Guess a 5 letter word!
+* Note: My algorithm for giving hints (e.g., the `*` characters in my_wordle) differs slightly from the original Wordle's, but is still correct
+* E.g., for the secret word `moist` and guess word `tests`, the following examples both hint for the letters `t` and `s`, but in different locations of `tests` :
+
+**Wordle:**
+<img src="./images/eg_wordle.png" alt="Wordle example" width="400"/>
+
+**my_wordle:**
+
+<img src="./images/eg_my_wordle.png" alt="my wordle example" width="175"/> &nbsp;
+
+where `⏟` indicates gray boxes, `*` indicates yellow boxes, and the actual letters (e.g., `s`) indicate green boxes.
+
+
+For copying results to Twitter (or other social media), `□` represents correct characters (hidden for others)
+
+* E.g.,
+
+<img src="./images/wordle_twitter.png" alt="Wordle example" width="200"/>
+&emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+<img src="./images/my_wordle_twitter.png" alt="Wordle example" width="200"/>
--- my_wordle.py
+++ my_wordle.py
@@ -2,17 +2,40 @@ import sys
 import enchant
 from getpass import getpass
 from collections import Counter
+from random_word import RandomWords
+import time
+
+# Replace alphabetical character with □ character
+def replace_alpha(s):
+  res = []
+  for c in s:
+    if c.isalpha():
+      res.append("□")
+    else:
+      res.append(c)
+  return "".join(res)
 
 d = enchant.Dict("en_US")
 
 print("WORDLE, THE SEQUEL")
-print("(\'quit\' to exit)\n")
+print("(\'q\' to exit)\n")
 
-word = getpass("Secret word: ")
-print()
+# User can input the secret word
+# word = getpass("Secret word: ")
+# print()
+
+# Get a random 5 letter word
+r = RandomWords().get_random_word(minLength=5, maxLength=5)
+# Make sure secret word is valid pyenchant library recognizes it
+while not r or not r.isalpha() or not d.check(r.lower()):
+  time.sleep(0.00005)
+  r = RandomWords().get_random_word(minLength=5, maxLength=5)
+word = r.lower()
+
+# print(word)
 
 # Exit program
-if word == 'quit':
+if word == 'q':
   sys.exit()
 
 while len(word) != 5 or not d.check(word):
@@ -26,28 +49,30 @@ while len(word) != 5 or not d.check(word):
       pass # Handeled by the first if-statement
   word = getpass("Secret word: ")
   # Exit program
-  if word == 'quit':
+  if word == 'q':
     sys.exit()
   print()
   
-print("Word accepted. Guess the word below in 6 tries!")
+print("Guess the word in 6 tries!")
 print("Hint: { ⏟ : wrong char, * : right char and wrong place }\n")
 
-correct_placement = {}
+correct_placements = {}
 for i, c in enumerate(word):
-  if c in correct_placement:
-    correct_placement[c].add(i)
+  if c in correct_placements:
+    correct_placements[c].add(i)
   else:
-    correct_placement[c] = set([i])
+    correct_placements[c] = set([i])
   
 
 hint = []
+hint_history = []
 count = 0
+win_flag = False
 # Guess the word
 while count < 6 and "".join(hint) != word:
   guess = input()
   # Exit program
-  if guess == 'quit':
+  if guess == 'q':
     sys.exit()
   
   while len(guess) != 5 or not d.check(guess):
@@ -61,29 +86,59 @@ while count < 6 and "".join(hint) != word:
         pass
     guess = input()
     # Exit program
-    if guess == 'quit':
+    if guess == 'q':
       sys.exit()
     
   if guess == word:
+    print(guess+"\n")
+    hint_history.append(word)
+    win_flag = True
+    count += 1
     break
   
-  letter_count = Counter()
+  # First pass
+  guess_counts = Counter()
+  for c in guess:
+    guess_counts[c] += 1
+  
+  letter_counts = Counter()
+  # Second pass
   for i, c in enumerate(guess):
-    letter_count[c] += 1
+    letter_counts[c] += 1
+    # Case where i is the correct index of the secret word
     if c==word[i]:
       hint.append(c)
     elif c in word:
-      if letter_count[c] <= word.count(c) and i in correct_placement[c]:
+      if guess_counts[c] <= word.count(c):
         hint.append("*")
+      elif guess_counts[c] > word.count(c) and i not in correct_placements[c]:
+        if letter_counts[c] > word.count(c):
+          hint.append("*")
+        else:
+          hint.append("⏟")
       else:
         hint.append("⏟")
     else:
       hint.append("⏟")
-  print("".join(hint)+"\n")
+  h = "".join(hint)
+  hint_history.append(h)
+  print(h+"\n")
   hint = []
   count += 1
 
-if count == 6:
-  print("Sorry, you failed 😔")
+if count >= 6:
+  if win_flag:
+    print("Congrats, you got the word!\n")
+  else:
+    print("Sorry, you failed 😔")
+    print("The word was {}\n".format(word))
 else:
-  print("Congrats, you got the word!")
+  print("Congrats, you got the word!\n")
+  
+print("Post to Twitter!\n")
+print("my_wordle, {}/6\n".format(count))
+
+for i in hint_history:
+  print(replace_alpha(i))
+  
+print()
