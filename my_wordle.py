631478df06abf6cda45f6fe1c508fe1b98ee8298
bryan22lee <bryan22lee@gmail.com>
2022-02-02 20:53:28 -0600

    breaks with the word tacos

--- my_wordle.py
+++ my_wordle.py
@@ -16,10 +16,6 @@ if word == 'quit':
   sys.exit()
 
 while len(word) != 5 or not d.check(word):
-  # Exit program
-  if word == 'quit':
-    sys.exit()
-  
   if len(word) != 5:
     print("Word should have 5 characters\n")
   else:
@@ -29,20 +25,30 @@ while len(word) != 5 or not d.check(word):
     except:
       pass # Handeled by the first if-statement
   word = getpass("Secret word: ")
+  # Exit program
+  if word == 'quit':
+    sys.exit()
   print()
   
 print("Word accepted. Guess the word below in 6 tries!")
 print("Hint: { ⏟ : wrong char, * : right char and wrong place }\n")
 
+correct_placement = {}
+for i, c in enumerate(word):
+  if c in correct_placement:
+    correct_placement[c].add(i)
+  else:
+    correct_placement[c] = set([i])
+  
+
 hint = []
 count = 0
 # Guess the word
 while count < 6 and "".join(hint) != word:
-  # Exit program
-  if word == 'quit':
-    sys.exit()
-  
   guess = input()
+  # Exit program
+  if guess == 'quit':
+    sys.exit()
   
   while len(guess) != 5 or not d.check(guess):
     if len(guess) != 5:
@@ -54,6 +60,9 @@ while count < 6 and "".join(hint) != word:
       except:
         pass
     guess = input()
+    # Exit program
+    if guess == 'quit':
+      sys.exit()
     
   if guess == word:
     break
@@ -64,7 +73,7 @@ while count < 6 and "".join(hint) != word:
     if c==word[i]:
       hint.append(c)
     elif c in word:
-      if letter_count[c] <= word.count(c):
+      if letter_count[c] <= word.count(c) and i in correct_placement[c]:
         hint.append("*")
       else:
         hint.append("⏟")
